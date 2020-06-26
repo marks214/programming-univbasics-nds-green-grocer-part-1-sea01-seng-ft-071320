4b240a3b1fb39fc793d368d49b96354d97e694f0
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-26 00:23:41 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -4,9 +4,9 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   collection.each do |aoh|
     aoh.each do |key, value|
-    end
 puts key
 puts value
+end
 
   end
 
